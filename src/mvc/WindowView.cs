d29359446ff0988216c0594268f9d0a738757cce
mattmaniak <2378768-mattmaniak@users.noreply.gitlab.com>
2020-08-13 17:08:06 +0200

    Check window size and inform if it's too small

--- src/mvc/WindowView.cs
+++ src/mvc/WindowView.cs
@@ -14,39 +14,45 @@ namespace fdte
 		private const char _lowerBarBorder = '_';
 		private const string _upperBarTitle = "Five Day Text Editor";
 		private static readonly int _minWidth = _upperBarTitle.Length;
+		private static readonly int _minHeight = (_barHeight * _barsNumber) + _textMinRenderedLines;
 
 		private const int _barHeight = 1;
 		private const int _barsNumber = 2;
+		private const int _textMinRenderedLines = 1;
 
 		private static int Width
 		{
-			get { return TryToRetreiveWindowAxis(Axis.X); }
+			get { return TryToRetreiveWindowAxisSize(Axis.X); }
 		}
 
 		private static int Height
 		{
-			get { return TryToRetreiveWindowAxis(Axis.Y); }
+			get { return TryToRetreiveWindowAxisSize(Axis.Y); }
 		}
 
 		public static void Render()
 		{
-			DrawUpperBar();
-			for (int y = 0; y < Height - (_barHeight * _barsNumber) - TextProcessorModel.Text.Count; y++)
+			if (IsConsoleWindowBigEnough())
 			{
-				if (y < TextProcessorModel.Text.Count)
+				DrawUpperBar();
+
+				for (int y = 0; y < Height - (_barHeight * _barsNumber) - TextProcessorModel.Text.Count; y++)
 				{
-					Console.Write(TextProcessorModel.Text[y]);
-					if (y == TextProcessorModel.Text.Count - 1)
+					if (y < TextProcessorModel.Text.Count)
 					{
-						Console.WriteLine(_cursor);
+						Console.Write(TextProcessorModel.Text[y]);
+						if (y == TextProcessorModel.Text.Count - 1)
+						{
+							Console.WriteLine(_cursor);
+						}
 					}
+					Console.WriteLine();
 				}
-				Console.WriteLine();
+				DrawLowerBar();
 			}
-			DrawLowerBar();
 		}
 
-		private static int TryToRetreiveWindowAxis(Axis axis)
+		private static int TryToRetreiveWindowAxisSize(Axis axis)
 		{
 			int axisValue = 0;
 
@@ -82,6 +88,18 @@ namespace fdte
 		private static void DrawUpperBar()
 		{
 			Console.WriteLine(_upperBarTitle);
+		}
+
+		private static bool IsConsoleWindowBigEnough()
+		{
+			if ((Width < _minWidth) || (Height < _minHeight))
+			{
+				Console.Clear();
+				Console.WriteLine("FDTE: too small window.");
+
+				return false;
+			}
+			return true;
 		}
 	}
 }
